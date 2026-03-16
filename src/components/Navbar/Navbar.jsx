5535dcd8f8b494d3a618f1003075701b39372886
Adam Odienne <odienne.adam@gmail.com>
2026-03-16 11:52:00 +0100

    fix backdrop filter

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,8 @@ export const Navbar = () => {
         navTween.fromTo(
             "nav", {backgroundColor: 'transparent'},
             {
-                backgroundColor: "#00000050", backgroundFilter: 'blur(10px)',
+                backgroundColor: "#00000050",
+                backdropFilter: 'blur(10px)',
                 duration: 1, ease: 'power1.inOut'
             })
     });
