48c1b76bd5ed402e2791259045622a2f298fdeea
Adam Odienne <odienne.adam@gmail.com>
2026-03-17 10:14:19 +0100

    refactor components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import {Navbar} from "./components/Navbar/Navbar.jsx";
-import {Hero} from "./components/Navbar/Hero.jsx";
-import {Cocktails} from "./components/Navbar/Cocktails.jsx";
-import {About} from "./components/Navbar/About.jsx";
-import {Art} from "./components/Navbar/Art.jsx";
-import {Menu} from "./components/Navbar/Menu.jsx";
-import {Contact} from "./components/Navbar/Contact.jsx";
+import {Hero} from "./components/Hero/Hero.jsx";
+import {Cocktails} from "./components/Cocktails/Cocktails.jsx";
+import {About} from "./components/About/About.jsx";
+import {Art} from "./components/Art/Art.jsx";
+import {Menu} from "./components/Menu/Menu.jsx";
+import {Contact} from "./components/Contact/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
